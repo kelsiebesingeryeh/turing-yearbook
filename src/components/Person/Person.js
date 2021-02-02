5685a54287091455b8d649d05a12e6488c997565
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-02-01 17:56:53 -0700

    Add styling for yearbook container and cards

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -4,7 +4,7 @@ import './Person.css';
 const Person = (props) => {
     return (
         <div className="staffCards">
-            <img src={props.people.photo} alt="headshot" />
+            <img className='birdImage' src={props.people.photo} alt="headshot" />
             <p style={{color: 'red', fontWeight: 'bold'}}>{props.people.name}</p>
             <p style={{fontStyle: 'italic'}}>{props.people.quote}</p>
             <p style={{color: 'blue', fontWeight: 'bold'}}>{props.people.superlative}</p>
