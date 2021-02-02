fc501003fbc5cb936c482f1609a4d165171e7609
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-02-01 17:19:07 -0700

    Add styling for card component

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -3,11 +3,11 @@ import './Person.css';
 
 const Person = (props) => {
     return (
-        <div>
+        <div className="staffCards">
             <img src={props.people.photo} alt="headshot" />
-            <p>{props.people.name}</p>
-            <p>{props.people.quote}</p>
-            <p>{props.people.superlative}</p>
+            <p style={{color: 'red', fontWeight: 'bold'}}>{props.people.name}</p>
+            <p style={{fontStyle: 'italic'}}>{props.people.quote}</p>
+            <p style={{color: 'blue', fontWeight: 'bold'}}>{props.people.superlative}</p>
         </div>
     )
 }
